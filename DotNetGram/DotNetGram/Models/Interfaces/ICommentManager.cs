373c6b9ec7ba453c2f221114ffdd2bbd271263f5
RickFlinn <mriodine@gmail.com>
2019-02-13 12:13:48 -0800

    Initialized solution, and added service/database dependencies.

---
+++ DotNetGram/DotNetGram/Models/Interfaces/ICommentManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetGram.Models.Interfaces
+{
+    interface ICommentManager
+    {
+        Task SaveAsync(Comment comment);
+
+        Task<Comment> GetAsync(int commentID);
+
+        Task<IEnumerable<Comment>> GetForPostAsync(int postID);
+
+        Task<IEnumerable<Comment>> GetAllAsync();
+
+        Task DeleteAsync(int commentID);
+    }
+}
---
+++ DotNetGram/DotNetGram/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetGram.Models
+{
+    public class Post
+    {
+        // Primary Key
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        // To be replaced
+
+        public string ImageUrl { get; set; }
+
+
+        // Navigation Properties
+        public List<Comment> Comments { get; set; }
+    }
+}
